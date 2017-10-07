9c08b55ce26666a81895bc88dcdc3f58bd84e573
sgarcia22 <sgarcia22@ufl.edu>
2017-10-07 13:18:17 -0400

    Started Database script

---
+++ Assets/Scripts/PatientInfo.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PatientInfo : MonoBehaviour {
+
+	//Patient Information
+	public string firstName;
+	public string lastName;
+	public int MRN;
+	public string medicalHistory;
+	public string treatmentPlan;
+	public string medication;
+
+	//public List<List<string>> patients;
+
+	void Awake () {
+
+
+
+	}
+}
