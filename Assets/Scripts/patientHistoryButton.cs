43a0c6885e1ab41c795f6c09ffc8675608b59bf3
julietDiBenedetto <jdibenedetto@ufl.edu>
2017-10-07 17:30:29 -0400

    reduced size UI

---
+++ Assets/Scripts/patientHistoryButton.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class patientHistoryButton : MonoBehaviour {
+
+	// Use this for initialization
+        
+    public Button yourButton;
+
+   public void Start()
+    {
+        Button btn = yourButton.GetComponent<Button>();
+        btn.onClick.AddListener(TaskOnClick);
+    }
+
+    void TaskOnClick()
+    {
+        Debug.Log("You have clicked the button!");
+    }
+
+    
+}
