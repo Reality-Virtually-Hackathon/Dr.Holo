8b171c8ff24bc3f4a1673fc3cc308d01e7408a41
Alec Hoffmann <astrohoff@ufl.edu>
2017-10-08 09:47:41 -0400

    Started working on patient info json stuff.

---
+++ Assets/Scripts/PatientInfoManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+public class PatientInfoManager : MonoBehaviour {
+    List<PatientInfo> patientInfos;
+
+	void Start () {
+        patientInfos = new List<PatientInfo>();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
