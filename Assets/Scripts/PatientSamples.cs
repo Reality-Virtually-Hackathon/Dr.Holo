284203ce26b87f31a4c56306b08e3ee3e116b2e3
DESKTOP-27VF8O1\denny <dennys1234pelegrin@gmail.com>
2017-10-07 22:20:52 -0400

    Read from file

--- Assets/Scripts/PatientSamples.cs
+++ Assets/Scripts/PatientSamples.cs
@@ -1,61 +1,105 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Text;
+using System.IO;
+using System;
+using System.Linq;
+
 
 public class PatientSamples : MonoBehaviour {
 
-	public void createPatients () {
+	public void createPatients ()
+    {
+        try
+        {
+            string fileAddress = "C:\\Users\\denny\\Desktop\\ReadWriteTest.txt";
+            string line;
+            StreamReader theReader = new StreamReader(fileAddress, Encoding.Default);
+            using (theReader)
+            {
+                do
+                {
+                    line = theReader.ReadLine();
+                    if (line != null)
+                    {
+                        //below here is where our stuff would go
 
-		PatientInfo sam = new PatientInfo();
-		sam.firstName = "sam";
-		sam.lastName = "garcia";
-		sam.MRN = 513425;
-		sam.medicalHistory = "Lactose";
-		sam.medication = "Lactose pills";
-		sam.treatmentPlan = "N/A";
-		//sam.profilePics = (Image)Resources.Load("Sam_Face");
-		GetPatients.patients.Add(sam);
+                        string[] entries = line.Split(',');
+                        if (entries.Length > 0)
+                        {                 
+                            PatientInfo patient = new PatientInfo();
+                            patient.firstName = entries[0];             //first name
+                            patient.lastName = entries[1];              //last name
+                            int putitin = Int32.Parse(entries[2]);      
+                            patient.MRN = putitin;                      //MRN
+                            patient.medicalHistory = entries[3];        //medical history
+                            patient.medication = entries[4];            //medication
+                            patient.treatmentPlan = entries[5];         //treatment plan
+                            GetPatients.patients.Add(patient);
+                        }
+                    }
+                }
+                while (line != null);
+                theReader.Close();
+                //return true;
+            }
+        }
+        catch (Exception e)
+        {
+           Console.WriteLine("{0}\n", e.Message);
+           //return false;
+        }
+    }
+		//PatientInfo sam = new PatientInfo();
+		//sam.firstName = "sam";
+		//sam.lastName = "garcia";
+		//sam.MRN = 513425;
+		//sam.medicalHistory = "Lactose";
+		//sam.medication = "Lactose pills";
+		//sam.treatmentPlan = "N/A";
+		////sam.profilePics = (Image)Resources.Load("Sam_Face");
+		//GetPatients.patients.Add(sam);
 
-		PatientInfo dennys = new PatientInfo();
-		dennys.firstName = "dennys";
-		dennys.lastName = "pelegrin";
-		dennys.MRN = 748028;
-		dennys.medicalHistory = "N/A";
-		dennys.medication = "N/A";
-		dennys.treatmentPlan = "Healthy";
-		//dennys.profilePics = (Image)Resources.Load("Sam_Face");
-		GetPatients.patients.Add(dennys);
+		//PatientInfo dennys = new PatientInfo();
+		//dennys.firstName = "dennys";
+		//dennys.lastName = "pelegrin";
+		//dennys.MRN = 748028;
+		//dennys.medicalHistory = "N/A";
+		//dennys.medication = "N/A";
+		//dennys.treatmentPlan = "Healthy";
+		////dennys.profilePics = (Image)Resources.Load("Sam_Face");
+		//GetPatients.patients.Add(dennys);
 
-		PatientInfo alec = new PatientInfo();
-		alec.firstName = "alec";
-		alec.lastName = "hoffmann";
-		alec.MRN = 589248;
-		alec.medicalHistory = "Asthma";
-		alec.medication = "Inhaler";
-		alec.treatmentPlan = "N/A";
-		//alec.profilePics = (Image)Resources.Load("Sam_Face");
-		GetPatients.patients.Add(alec);
+		//PatientInfo alec = new PatientInfo();
+		//alec.firstName = "alec";
+		//alec.lastName = "hoffmann";
+		//alec.MRN = 589248;
+		//alec.medicalHistory = "Asthma";
+		//alec.medication = "Inhaler";
+		//alec.treatmentPlan = "N/A";
+		////alec.profilePics = (Image)Resources.Load("Sam_Face");
+		//GetPatients.patients.Add(alec);
 
-		PatientInfo abel = new PatientInfo();
-		abel.firstName = "abel";
-		abel.lastName = "paguio";
-		abel.MRN = 830473;
-		abel.medicalHistory = "high blood pressure";
-		abel.medication = "pills";
-		abel.treatmentPlan = "healthy foods";
-		//abel.profilePics = (Image)Resources.Load("Sam_Face");
-		GetPatients.patients.Add(abel);
+		//PatientInfo abel = new PatientInfo();
+		//abel.firstName = "abel";
+		//abel.lastName = "paguio";
+		//abel.MRN = 830473;
+		//abel.medicalHistory = "high blood pressure";
+		//abel.medication = "pills";
+		//abel.treatmentPlan = "healthy foods";
+		////abel.profilePics = (Image)Resources.Load("Sam_Face");
+		//GetPatients.patients.Add(abel);
 
-		PatientInfo juliet = new PatientInfo();
-		juliet.firstName = "juliet";
-		juliet.lastName = "dibs";
-		juliet.MRN = 513425;
-		juliet.medicalHistory = "PTSD";
-		juliet.medication = "pills";
-		juliet.treatmentPlan = "therapy";
-		//juliet.profilePics = (Image)Resources.Load("Sam_Face");
-
-
-	}
+		//PatientInfo juliet = new PatientInfo();
+		//juliet.firstName = "juliet";
+		//juliet.lastName = "dibs";
+		//juliet.MRN = 513425;
+		//juliet.medicalHistory = "PTSD";
+		//juliet.medication = "pills";
+		//juliet.treatmentPlan = "therapy";
+		////juliet.profilePics = (Image)Resources.Load("Sam_Face");
+        
 
 }
+
