cf847acd89736b475b6a424591fe8e49452bfedd
abel <rakuhana@ufl.edu>
2017-10-08 14:24:57 -0400

    new UI

---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class Menu : MonoBehaviour {
+
+    public List<GameObject> pages = new List<GameObject>();
+    public int currentPageIndex = 0;
+    public GameObject currentPage;
+    public CanvasGroup cg;
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+
+    public void NewPage(int newPage)
+    {
+        if(newPage != currentPageIndex)
+            StartCoroutine("ChangePage", newPage);
+    }
+
+    public IEnumerator ChangePage(int newPage)
+    {
+        //Close page
+        currentPage.SetActive(false);
+
+        //Open new page
+        currentPageIndex = newPage;
+        currentPage = pages[currentPageIndex];
+        currentPage.SetActive(true);
+
+        yield return 0;
+
+    }
+
+
+
+
+}
+
